2e246282b127efccbea3d4ecf4f4687140ba88b1
Numpy <vgl.marcos@gmail.com>
2020-06-23 18:57:11 -0500

    hmm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
 // Mis componentes
+import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { SignInComponent } from './components/signIn/signIn.component';
